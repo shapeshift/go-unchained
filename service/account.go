ec8c766775abe0098cd6b53e13b0fa7370360d4e
Sean Luther <elmutt@gmail.com>
2021-12-10 16:18:15 -0700

    balances grpc

--- service/account.go
+++ service/account.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 
 	"github.com/cosmos/cosmos-sdk/codec/types"
+	cosmos_cosmos_sdk_types "github.com/cosmos/cosmos-sdk/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -30,47 +32,28 @@ type CosmosAccountResponse struct {
 	Account *CosmosAccount `json:"account"`
 }
 
-type CosmosBalance struct {
-	Denom  string `json:"denom"`
-	Amount string `json:"amount"`
-}
-
-type CosmosBalancesResponse struct {
-	Balances   []CosmosBalance  `json:"balances"`
-	Pagination CosmosPagination `json:"pagination"`
-}
-
-type CosmosDelegation struct {
-	DelegatorAddress string `json:"delegator_address"`
-	ValidatorAddress string `json:"validator_address"`
-	Shares           string `json:"shares"`
-}
-
-type CosmosDelegationWrapper struct {
-	Delegation CosmosDelegation
-	Balance    CosmosBalance
-}
-
-type CosmosDelegationsResponse struct {
-	DelegationResponses []CosmosDelegationWrapper `json:"delegation_responses"`
-	Pagination          CosmosPagination          `json:"pagination"`
-}
-
 type Delegation struct {
 	Validator string `json:"validator"`
 	Amount    string `json:"amount"`
 	Shares    string `json:"shares"`
 }
 
-func (c *CosmosService) readDelegations(address string) ([]CosmosDelegationWrapper, error) {
-	res := &CosmosDelegationsResponse{}
-	if err := doGet(c.cosmosLightClient, fmt.Sprintf("/cosmos/staking/v1beta1/delegations/%s", address), nil, res); err != nil {
-		return nil, fmt.Errorf("error requesting delegations for %s: %s", address, err)
+func (c *CosmosService) readDelegations(address string) ([]stakingtypes.DelegationResponse, error) {
+
+	delegationClient := stakingtypes.NewQueryClient(c.grpcConn)
+	delegationRes, err := delegationClient.DelegatorDelegations(
+		context.Background(),
+		&stakingtypes.QueryDelegatorDelegationsRequest{DelegatorAddr: address},
+	)
+	if err != nil {
+		log.Errorf("grpc balances error", err)
+		return nil, err
 	}
-	return res.DelegationResponses, nil
+
+	return delegationRes.DelegationResponses, nil
 }
 
-func (c *CosmosService) readBalances(address string) ([]CosmosBalance, error) {
+func (c *CosmosService) readBalances(address string) (cosmos_cosmos_sdk_types.Coins, error) {
 	bankClient := banktypes.NewQueryClient(c.grpcConn)
 	balanceRes, err := bankClient.AllBalances(
 		context.Background(),
@@ -81,16 +64,7 @@ func (c *CosmosService) readBalances(address string) ([]CosmosBalance, error) {
 		return nil, err
 	}
 
-	cosmoBalances := make([]CosmosBalance, len(balanceRes.Balances))
-
-	for i, bal := range balanceRes.Balances {
-		cosmoBalances[i] = CosmosBalance{
-			Denom:  bal.Denom,
-			Amount: bal.Amount.String(),
-		}
-	}
-
-	return cosmoBalances, nil
+	return balanceRes.Balances, nil
 }
 
 func (c *CosmosService) readAccount(address string) (*authtypes.BaseAccount, error) {
@@ -129,23 +103,25 @@ func (c *CosmosService) GetAccount(address string) (*Account, error) {
 	} else {
 		for _, b := range cosmosBals {
 			if b.Denom == "uatom" {
-				balance = string(b.Amount)
+				balance = string(b.Amount.String())
 			}
-			t := TokenAmount{Denom: b.Denom, Amount: b.Amount}
+			t := TokenAmount{Denom: b.Denom, Amount: b.Amount.String()}
 			tokens = append(tokens, t)
 		}
 	}
 
 	delegations := make([]Delegation, 0, 25)
+
 	cosmosDelegations, err := c.readDelegations(address)
+
 	if err != nil {
 		log.Errorf("error getting delegations for %s: %s", address, err)
 	} else {
 		for _, cd := range cosmosDelegations {
 			d := Delegation{
 				Validator: cd.Delegation.ValidatorAddress,
-				Shares:    cd.Delegation.Shares,
-				Amount:    cd.Balance.Amount,
+				Shares:    cd.Delegation.Shares.String(),
+				Amount:    cd.Balance.Amount.String(),
 			}
 			delegations = append(delegations, d)
 		}
